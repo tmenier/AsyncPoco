83d3f599105dc9350f00fb5924ff9a7e8c7adff6
tmenier <tmenier@gmail.com>
2014-01-27 22:46:00 -0600

    NuGet stuff

--- AsyncPoco/PostBuild.bat
+++ AsyncPoco/PostBuild.bat
@@ -1,8 +1,3 @@
-cd ..\..
-pwd
-
-
-if %1 == Release NuGet.exe pack "AsyncPoco.nuspec" -o "..\Output"
-if %1 == Release NuGet.exe pack "AsyncPoco.Core.nuspec" -o "..\Output"
+..\.nuget\nuget pack "NuGet\AsyncPoco.nuspec" -o "..\Output"
 
 ..\csj\csj.exe -o:AsyncPoco.cs Database.cs -r *.cs -x:Properties\*.cs
