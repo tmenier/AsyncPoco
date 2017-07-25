34533985f0211e881fa49e136adfff361f0cbfcd
Lee Grissom <leegrissom@gmail.com>
2017-07-25 10:10:13 -0700

    Restore indentation to Tabs
    Just restored indentations to Tabs for consistency purposes. It's not a preference one way or another.

--- AsyncPoco.Tests/Misc.cs
+++ AsyncPoco.Tests/Misc.cs
@@ -3,50 +3,50 @@ using System.Collections.Generic;
 
 namespace AsyncPoco.Tests
 {
-    [TestFixture]
-    class Misc
-    {
-        Database db = new Database("sqlserver");
+	[TestFixture]
+	class Misc
+	{
+		Database db = new Database("sqlserver");
 
-        [Test]
-        public void EscapeColumnName()
-        {
-            Assert.AreEqual(db._dbType.EscapeSqlIdentifier("column.name"), "[column.name]");
-            Assert.AreEqual(db._dbType.EscapeSqlIdentifier("column name"), "[column name]");
-        }
+		[Test]
+		public void EscapeColumnName()
+		{
+			Assert.AreEqual(db._dbType.EscapeSqlIdentifier("column.name"), "[column.name]");
+			Assert.AreEqual(db._dbType.EscapeSqlIdentifier("column name"), "[column name]");
+		}
 
-        [Test]
-        public void EscapeTableName()
-        {
-            Assert.AreEqual(db._dbType.EscapeTableName("column.name"), "column.name");
-            Assert.AreEqual(db._dbType.EscapeTableName("column name"), "[column name]");
-        }
+		[Test]
+		public void EscapeTableName()
+		{
+			Assert.AreEqual(db._dbType.EscapeTableName("column.name"), "column.name");
+			Assert.AreEqual(db._dbType.EscapeTableName("column name"), "[column name]");
+		}
 
-        enum Fruits
-        {
-            Apples,
-            Pears,
-            Bananas
-        }
+		enum Fruits
+		{
+			Apples,
+			Pears,
+			Bananas
+		}
 
-        enum Fruits2
-        {
-            Oranges
-        }
+		enum Fruits2
+		{
+			Oranges
+		}
 
-        [Test]
-        public void EnumMapper()
-        {
-            Assert.AreEqual(Fruits.Apples, Internal.EnumMapper.EnumFromString(typeof(Fruits), "Apples"));
-            Assert.AreEqual(Fruits.Pears, Internal.EnumMapper.EnumFromString(typeof(Fruits), "pears"));
-            Assert.AreEqual(Fruits.Bananas, Internal.EnumMapper.EnumFromString(typeof(Fruits), "BANANAS"));
-            Assert.AreEqual(Fruits2.Oranges, Internal.EnumMapper.EnumFromString(typeof(Fruits2), "Oranges"));
+		[Test]
+		public void EnumMapper()
+		{
+			Assert.AreEqual(Fruits.Apples, Internal.EnumMapper.EnumFromString(typeof(Fruits), "Apples"));
+			Assert.AreEqual(Fruits.Pears, Internal.EnumMapper.EnumFromString(typeof(Fruits), "pears"));
+			Assert.AreEqual(Fruits.Bananas, Internal.EnumMapper.EnumFromString(typeof(Fruits), "BANANAS"));
+			Assert.AreEqual(Fruits2.Oranges, Internal.EnumMapper.EnumFromString(typeof(Fruits2), "Oranges"));
 
-            // nullable enums
-            Assert.AreEqual(Fruits.Apples, Internal.EnumMapper.EnumFromString(typeof(Fruits?), "Apples"));
-            Assert.AreEqual(null, Internal.EnumMapper.EnumFromString(typeof(Fruits?), null));
+			// nullable enums
+			Assert.AreEqual(Fruits.Apples, Internal.EnumMapper.EnumFromString(typeof(Fruits?), "Apples"));
+			Assert.AreEqual(null, Internal.EnumMapper.EnumFromString(typeof(Fruits?), null));
 
-            Assert.Throws<KeyNotFoundException>(() => Internal.EnumMapper.EnumFromString(typeof(Fruits2), "Apples"));
-        }
-    }
+			Assert.Throws<KeyNotFoundException>(() => Internal.EnumMapper.EnumFromString(typeof(Fruits2), "Apples"));
+		}
+	}
 }
